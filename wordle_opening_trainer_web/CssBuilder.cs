b0d92b44790fa012bce3e4ef1f519f6af2dc2edc
erik-saltwell <169001579+erik-saltwell@users.noreply.github.com>
2025-07-18 20:15:16 -0700

    Moved game tiles to component, added style for different response types

---
+++ wordle_opening_trainer_web/CssBuilder.cs
@@ -0,0 +1,35 @@
+﻿namespace wordle_opening_trainer_web
+{
+    public class CssBuilder
+    {
+        private readonly HashSet<string> _classes = new(StringComparer.OrdinalIgnoreCase);
+
+        public CssBuilder Add(string @class, bool when = true)
+        {
+            if (when && !string.IsNullOrWhiteSpace(@class))
+            {
+                _classes.Add(@class);
+            }
+
+            return this;
+        }
+
+        public CssBuilder Remove(string @class)
+        {
+            if (!string.IsNullOrWhiteSpace(@class))
+            {
+                _classes.Remove(@class);
+            }
+
+            return this;
+        }
+
+        public CssBuilder Clear()
+        {
+            _classes.Clear();
+            return this;
+        }
+
+        public override string ToString() => string.Join(" ", _classes);
+    }
+}
---
+++ wordle_opening_trainer_web/LetterResponseTypes.cs
@@ -0,0 +1,12 @@
+﻿namespace wordle_opening_trainer_web
+{
+    public enum LetterResponseType
+    {
+        UNUSED,
+        UNFILLED,
+        UNSUBMITTED,
+        WRONG_LETTER,
+        WRONG_SPACE,
+        RIGHT
+    }
+}
